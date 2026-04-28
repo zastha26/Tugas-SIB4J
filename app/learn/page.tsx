4ecf48bf700a735e692205f7fcb48f06fe7988f8
zastha26 <zjuliansel@gmail.com>
2026-04-28 23:49:39 +0800

    first commit

---
+++ app/learn/page.tsx
@@ -0,0 +1,14 @@
+export default function Learn() {
+    return (
+        <div>
+            <div>
+                <img />
+                <p>Design</p>
+            </div>
+            <div>
+                <h1>Figma</h1>
+                <p>8 hours ago</p>
+            </div>
+        </div>
+    )
+}
