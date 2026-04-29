18b7be9ff6f90285eb08f01ce76224da4a7b6b2f
zastha26 <zjuliansel@gmail.com>
2026-04-29 18:23:46 +0800

    Add files via upload

---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+export default function Blog() {
+    return (
+        <div>
+            <p>Ini adalah halaman BLOG</p>
+            <input type="text" placeholder="Masukkan text disini" />
+            <button>Click Me</button>
+        </div>
+    )
+}
