9533fa4c9d94b1d1816704943cae429a6054bc26
zastha26 <zjuliansel@gmail.com>
2026-05-06 22:00:37 +0800

    Add files via upload

---
+++ app/cards/page.tsx
@@ -0,0 +1,13 @@
+export default function CardsPage() {
+    return (
+        <div className="flex justify-between p-5 item-center">
+            <div className="bg-gray-300 p-6 flex flex-col items-center rounded-2xl"> 
+                <div className="bg-white-100 h-72 p-6 flex flex-col justify-between rounded-2xl">
+                    <div className="bg-blue">
+
+                    </div>
+                  </div> 
+            </div>
+        </div>
+    )
+}
